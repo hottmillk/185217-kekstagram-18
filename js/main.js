03f6d66f73ddd1b808eabdf1461118a78cb72576
hottmillk <baranko555@gmail.com>
2019-10-19 15:13:26 +0300

    Обработка событий, первая проверка

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ var COMMENTS = [
 ];
 var NAMES = ['Иван', 'Антон', 'Саша', 'Дима', 'Петя'];
 var NUM_PHOTOS = 25;
+var ESC_KEYCODE = 27;
+var MAX_HASHTAGS = 5;
 
 var photos = [];
 
@@ -75,7 +77,7 @@ picturesBlock.appendChild(fragment);
 // Личный проект: больше деталей
 var bigPicture = document.querySelector('.big-picture');
 
-bigPicture.classList.remove('hidden');
+// временно скрыть bigPicture.classList.remove('hidden');
 
 bigPicture.querySelector('.big-picture__img > img').src = photos[0].url;
 bigPicture.querySelector('.likes-count').textContent = photos[0].likes;
@@ -96,3 +98,94 @@ var visuallyHidden = function (classString) {
 
 visuallyHidden('.social__comment-count');
 visuallyHidden('.social__comments-loader');
+
+
+// личный проект: Подробности
+// форма загрузки фото
+var uploadFileForm = document.querySelector('#upload-file');
+var editFileForm = document.querySelector('.img-upload__overlay');
+var cancelButton = editFileForm.querySelector('#upload-cancel');
+var photoHashtags = editFileForm.querySelector('.text__hashtags');
+var photoDescription = editFileForm.querySelector('.text__description');
+
+var openEditForm = function () {
+  editFileForm.classList.remove('hidden');
+  document.addEventListener('keydown', editFileFormEscHandler);
+};
+
+var closeEditForm = function () {
+  editFileForm.classList.add('hidden');
+  document.removeEventListener('keydown', editFileFormEscHandler);
+};
+
+var inputTagsDescriptionHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    evt.stopPropagation();
+  }
+};
+
+var editFileFormEscHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    editFileForm.classList.add('hidden');
+  }
+};
+
+uploadFileForm.addEventListener('change', openEditForm);
+cancelButton.addEventListener('click', closeEditForm);
+photoHashtags.addEventListener('keydown', inputTagsDescriptionHandler);
+photoDescription.addEventListener('keydown', inputTagsDescriptionHandler);
+
+// валидация
+photoHashtags.addEventListener('invalid', function () {
+  if (photoHashtags.validity.tooShort) {
+    photoHashtags.setCustomValidity('Слишком короткий хэш-тег');
+  }
+});
+
+var hashtagHandler = function () {
+  var hashtags = photoHashtags.value;
+
+  if (!hashtags.includes(' ')) {
+    return photoHashtags.setCustomValidity('Хэш-теги должны разделяться пробелами');
+  }
+
+  hashtags = hashtags.replace(/\s\s+/g, ' ').trim().toLowerCase().split(' ');
+
+  if (hashtags.length > MAX_HASHTAGS) {
+    return photoHashtags.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+  }
+
+  // eslint-disable-next-line no-shadow
+  for (var i = 0; i < hashtags.length; i++) {
+    if (hashtags[i][0] !== '#') {
+      return photoHashtags.setCustomValidity('Хэш-тег должен начинатся с символа #(решётка)');
+    } else if (hashtags[i].length > 20) {
+      return photoHashtags.setCustomValidity('Максимальная длина одного хэш-тега 20 символов, включая решётку');
+    } else if (hashtags.findIndex(function (tag, currentIndex) {
+      return tag === hashtags[i] && currentIndex !== i;
+    }) !== -1) {
+      return photoHashtags.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
+    }
+  }
+  return hashtags;
+};
+
+photoHashtags.addEventListener('change', hashtagHandler);
+
+photoHashtags.addEventListener('input', function () {
+  photoHashtags.setCustomValidity('');
+});
+
+// ГЛУБИНА ФИЛЬТРА
+
+var EFFECT_LEVEL_LINE = 455;
+var effectLevelPin = editFileForm.querySelector('.effect-level__pin');
+var effectLevelValue = editFileForm.querySelector('.effect-level__value');
+var effectLevelDepth = editFileForm.querySelector('.effect-level__depth');
+
+var effectLevelPinHandler = function () {
+  var currentEffectValue = Math.round(effectLevelDepth / (EFFECT_LEVEL_LINE / 100));
+  effectLevelValue.setAttribute('value', currentEffectValue);
+};
+
+effectLevelPin.addEventListener('mouseup', effectLevelPinHandler);
