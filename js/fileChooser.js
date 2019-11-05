de0ccf0c27f03ce9c087282e0105980b1b6a95fb
hottmillk <baranko555@gmail.com>
2019-11-05 17:49:47 +0200

    ветка для теста

---
+++ js/fileChooser.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function () {
+
+  var TYPE_FILE = ['gif', 'jpg', 'jpeg', 'png'];
+  var fileInput = document.querySelector('#upload-file');
+  var previewElement = document.querySelector('.img-upload__preview');
+  var previewImage = previewElement.querySelector('img');
+
+  var fileUploadChangeHandler;
+
+  var FileChooser = function (callback) {
+    fileUploadChangeHandler = callback;
+    fileInput.addEventListener('change', function () {
+      var file = fileInput.files[0];
+      if (!file) {
+        return;
+      }
+      if (checkFileType(file.name)) {
+        readFile(file);
+      } else {
+        window.utils.showError('Можно загрузить только файлы с расширением: ' + TYPE_FILE.toString());
+        clearInput();
+      }
+    });
+  };
+
+  var checkFileType = function (fileName) {
+    fileName = fileName.toLowerCase();
+    return TYPE_FILE.some(function (it) {
+      return fileName.endsWith(it);
+    });
+  };
+
+  var readFile = function (source) {
+    var reader = new FileReader();
+    reader.addEventListener('load', function () {
+      previewImage.src = reader.result;
+    });
+    reader.readAsDataURL(source);
+    clearPreview();
+    if (fileUploadChangeHandler && typeof (fileUploadChangeHandler) === 'function') {
+      fileUploadChangeHandler();
+    }
+  };
+
+  var clearPreview = function () {
+    previewImage.src = '';
+  };
+
+  var clearInput = function () {
+    fileInput.value = '';
+  };
+
+  FileChooser.prototype.clear = function () {
+    clearInput();
+    clearPreview();
+  };
+
+  window.FileChooser = FileChooser;
+})();
